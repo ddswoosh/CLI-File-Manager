6e4a4f8d03c0fcaf9f6fc74e5c4e7174f26c98e7
ddswoosh <ddswoosh@gmail.com>
2024-03-06 21:07:29 -0800

    Update, build out logic before fixing syntax.

--- src/cache.rs
+++ src/cache.rs
@@ -1,23 +1,41 @@
 use std::io;
 
-struct Node<T> {
-    val: T,
-    next: Option<Box<Node<T>>>,
-    prev: Option<Box<Node<T>>>,
+enum Action {
+    start,
+    mov,
+    nfil,
+    dfil,
+    ndir,
+    ddir,
+    grab,
+    open,
+    close,
+    search,
 }
 
-impl Node<String> {
-    fn new(val: String) -> Self {
+struct Node<T,Action> {
+    val: T,
+    action: Action,
+    next: Option<Box<Node<T, Action>>>,
+    prev: Option<Box<Node<T, Action>>>,
+}
+
+impl Node<String, String> {
+    fn new(val: String, action: Action) -> Self {
         Node {
             val: val,
+            action: action,
             next: None,
             prev: None
         }
+    
+        &mut count += 1;    
+
     }
 
 }
 
-fn retrieve(idx: usize, node: Option<Box<Node<String>>>) {
+fn retrieve(idx: usize, node: Option<Box<Node<String, Action>>>) {
 
     let mut input: String = String::new();
     let mut cur = node;
@@ -36,9 +54,50 @@ fn retrieve(idx: usize, node: Option<Box<Node<String>>>) {
 } 
 
 fn stage(check: String){
-    
+   let mut avail: bool = false;
+
+   let mut u_action: String = check;
+
+   match u_action {
+        Action::nfil => avail = true, _;
+        Action::dfil => avail = true, _;
+        Action::ndir => avail = true,_;
+        Action::ddir => avail = true,_;
+        Action::mov => avail = true, _;
+        _ => println!("Cannot revert this function type. New/Delete file | New/Delete Directory | Move, are the only supported functions for reversion.")
+   }
+   
+   if avail == true {
+        // run some fn
+   }
+   
+}
+
+fn move() {
+
+}
+
+fn newDirectory() {
+
+}
+
+fn removeDirectory() {
+
+}
+
+fn newFile() {
+
+}
+
+fn removeFile() {
+
 }
 
 pub fn run() {
-    let op1 = Node::new(String::new("History Start"));
+    let mut count: usize = 0;
+
+    let op1 = Node::new(String::new("History Start", Action::start));
+
+    let mut head = Some(op1);
+    let mut tail = Some(op1);
 }
