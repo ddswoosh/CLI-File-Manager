2a5852690e6c020d641178c7f2e15eb6a87d5d83
ddswoosh <ddswoosh@gmail.com>
2024-05-05 21:32:43 -0700

    .

--- src/controllers/controls.rs
+++ src/controllers/controls.rs
@@ -33,6 +33,7 @@ grab (store file/directory in an array) -> grab test.py
 show (display file in the grab array) -> show 
 drop (drop file/directory from array) -> drop 
 mov (move file/directory) -> mov test.py {or type holding to use grabbed file} new/test.py 
+mov (move file/directory) -> copy test.py new/test.py 
 cd (change directory) -> cd directory or cd back to pop up a level
 list (display all items in current directory) -> list
 addext (display all items in current directory) -> add javascript .js
